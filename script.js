d8055a399beee4344acd78d634b0405b13fcaab3
Noémie Galliot <noemie.galliot@free.fr>
2025-03-11 16:31:43 +0100

    afficher dans la consoles les touches cliquées

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Noémie Galliot
 // NOTE: 
 // This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+document.addEventListener("DOMContentLoaded", () => {
+    touches=document.getElementsByClassName("calculator__keys")
+    let number = touches[0].childNodes
+    ecran=document.getElementsByClassName("calculator__display")
+    number.forEach((e)=> e.addEventListener("click", ()=> console.log(e.innerHTML)))
+})
